8b2ac3db6a456f5b3704a7ec6757e5c19bb8d415
kaori-igawa <kaori0v0igawa@gmail.com>
2024-12-26 20:27:29 +0900

    threejs_dispersionページ削除

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -3,7 +3,6 @@
 export default function Main() {
     return (
         <div className="p-10">
-            <a className="text-xl block" href="/threejs_dispersion">threejs_dispersion</a>
             <a className="text-xl block" href="/city_camera_animation">city_camera_animation scroll all linked</a>
             <a className="text-xl block" href="/city_camera_animation02">city_camera_animation Partially linked</a>
         </div>
